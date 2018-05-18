bb272746469c6b71f961f471422aeff238599afb
Jean Felipe <jean.silva@consultaremedios.com.br>
2018-05-18 17:36:13 -0300

    bulma added to project

--- app/javascript/packs/components/Store/Store.vue
+++ app/javascript/packs/components/Store/Store.vue
@@ -1,5 +1,12 @@
 <template>
-  <p>Store</p>
+  <div>
+      <div class="header">
+        <span>AirSoft Market</span>
+        <span class="login">Entrar</span>
+      </div>
+      <input class="input" type="text" placeholder="Text input">
+  </div>
+
 </template>
 
 <script>
